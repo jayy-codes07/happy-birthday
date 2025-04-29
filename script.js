d157c190f42b44d24344616a7fb914914f6b93db
jayy-codes07 <hadiyajay2010@gmail.com>
2025-04-29 15:00:11 +0530

    script.js

--- script.js
+++ script.js
@@ -274,7 +274,7 @@ heart.addEventListener("touchend", triggerHeartTap);
   
   document.addEventListener("touchend", (e) => {
     const balloon = document.createElement("div");
-    balloon.textContent = "🎈";
+    balloon.textContent = "⭐️";
     balloon.style.position = "fixed";
     balloon.style.left = `${e.clientX}px`;
     balloon.style.top = `${e.clientY}px`;
